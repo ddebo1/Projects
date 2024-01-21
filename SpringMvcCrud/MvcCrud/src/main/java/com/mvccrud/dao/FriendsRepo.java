9f75c71a770fcc878822af3be5700ac5baa351f1
Deborah <deborah@gmail.com>
2024-01-21 16:58:55 +0530

    Revert "CRUD App using Spring MVC & Data JPA"
    This reverts commit b84806d304d944d7e8d4ad60624dd9ed549201b6.

--- SpringMvcCrud/MvcCrud/src/main/java/com/mvccrud/dao/FriendsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mvccrud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mvccrud.entity.Friends;
-
-public interface FriendsRepo extends JpaRepository<Friends, Integer> {
-
-}
